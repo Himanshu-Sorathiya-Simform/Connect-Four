bdefd257849916b5bad83675351636e1201aa074
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-05-07 14:38:26 +0530

    restructure the files and folders

--- helpers.d.ts
+++
@@ -1,5 +0,0 @@
-import type { User } from './script.js';
-declare function findLastUnmodified(columnNumber: number): false | HTMLDivElement | undefined;
-declare function isWin(elementId: string | undefined, user: User): boolean | undefined;
-export { findLastUnmodified, isWin };
-//# sourceMappingURL=helpers.d.ts.map
--- script.d.ts
+++
@@ -1,3 +0,0 @@
-type User = 'player1' | 'player2';
-export type { User };
-//# sourceMappingURL=script.d.ts.map
